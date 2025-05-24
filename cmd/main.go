e231bc55710adcdd5113635a9bcabcc487655c29
lieranderl <19622412+lieranderl@users.noreply.github.com>
2025-05-24 22:23:57 +0200

    supabase auth proxy

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/rs/zerolog"
 	"github.com/todoflow-labs/shared-dtos/logging"
 	"github.com/todoflow-labs/sse-updates/internal/config"
 	"github.com/todoflow-labs/sse-updates/internal/sse"
@@ -41,22 +43,29 @@ func main() {
 
 const UserIDKey string = "user_id"
 
-// type ctxKey string
+// Writes a structured JSON error
+func writeError(w http.ResponseWriter, status int, msg string) {
+	w.WriteHeader(status)
+	_ = json.NewEncoder(w).Encode(map[string]string{
+		"error": msg,
+	})
+}
 
-func authMiddleware(next http.Handler, logger logging.Logger) http.Handler {
+func authMiddleware(next http.Handler, logger zerolog.Logger) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		userID := r.Header.Get("X-User-ID")
-		logger.Debug().Str("X-User-ID from header", userID).Msg("authMiddleware")
 		if userID == "" {
-			if os.Getenv("ENV") == "development" {
+			if os.Getenv("ENV") != "production" {
+				logger.Info().Msg("Using test user ID")
 				userID = "test-user"
 			} else {
-				http.Error(w, "X-User-ID header required", http.StatusUnauthorized)
+				logger.Error().Msg("Missing X-User-ID header")
+				writeError(w, http.StatusUnauthorized, "Missing X-User-ID header")
 				return
 			}
 		}
-		logger.Debug().Str("user_id", userID).Msg("authMiddleware")
-		ctx := context.WithValue(r.Context(), UserIDKey, userID)
+		logger.Info().Msgf("User authenticated, %s", userID)
+		ctx := context.WithValue(r.Context(), "user_id", userID)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.3
 
 require (
 	github.com/nats-io/nats.go v1.42.0
+	github.com/rs/zerolog v1.34.0
 	github.com/todoflow-labs/shared-dtos v0.1.9
 )
 
@@ -13,7 +14,6 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/nats-io/nkeys v0.4.11 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
-	github.com/rs/zerolog v1.34.0 // indirect
 	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 )
